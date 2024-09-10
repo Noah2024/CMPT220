8e5889a4249ce322e95bf83bd40da3ebd8e432af
Noah2024 <139483586+Noah2024@users.noreply.github.com>
2024-09-10 13:29:54 -0400

    NextHomeworkDone

--- Homework 3/LoopsDebug.java
+++ Homework 3/LoopsDebug.java
@@ -1,11 +1,12 @@
+import java.util.Scanner;
 
-public class Main {
+public class LoopsDebug {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
 
         //P1: This one only prints 0-9, can you fix it so it prints 1-10?
         System.out.println("Problem 1");
-        for (int i = 0; i < 10; i++){
+        for (int i = 1; i < 11; i++){
             System.out.println(i);
         }
 
@@ -13,29 +14,48 @@ public class Main {
         //(factorial = X!, X being the user input, Factorials are every digit before X multiplied together)
         System.out.println("Problem 2");
         System.out.println("Enter a number and I will tell you the fatorial: ");
+        int input = Integer.parseInt(sc.nextLine());
         //here's a hint
-        for (int i = 0; i < ?; i++){
-
+        int sum = input;
+        for (int i = input-1; i > 0; i--){
+            sum *= i;
         }
+        System.out.print("Factorial is: ");
+        System.out.println(sum);
 
         //P3: Ask the user for a number, and then add together every OTHER digit (starting from 1)
         System.out.println("Problem 3");
         System.out.println("Enter a number and I will tell you the sum of every other number: ");
+        int input2 = Integer.parseInt(sc.nextLine());
+        int sum2 = 0;
         //No hint! what do you need to complete this task?
-
-
+        for (int i = 1; i<input2+1; i+=1){
+            sum2 += i;
+        }
+        System.out.print("Sum is: ");
+        System.out.println(sum2);
         //P4: Why does this loop never stop!
+        // Cause it was set to true
+        // Its like telling a child to to run then immedietley yelling at them for running
         //what can you do to break out of the loop after it prints once?
         System.out.println("Problem 4");
         boolean run = true;
         while (run == true){
             System.out.println("I printed once!");
+            run = false;
         }
 
         //P5: Take a string from the user and print them the reverse!
         System.out.println("Problem 5");
-        //hint
-        String reverse = "";
+        String input3 = sc.nextLine();
+       // String input3 = sc.nextLine();
+        String reverseInpt = "";
+        for (int i = input3.length(); i>0; i-=1){
+            reverseInpt += input3.substring(i-1, i);
+        }
+        System.out.print(reverseInpt);
+
+        //hint     String reverse = "";
 
     }
 }
--- Homework 4/Array.java
+++ Homework 4/Array.java
@@ -1,4 +1,4 @@
-
+import java.text.MessageFormat;
 /*
 Take the given array and keep track of:
 1. Any duplicate numbers
@@ -7,10 +7,22 @@ Take the given array and keep track of:
 
  */
 
-public class Main {
+public class Array {
     public static void main(String[] args) {
         //Don't let the size of this scare you! no matter how big an array is, it all works the same!
         int[] myArray = {10,3,295,38,20,3,4,267,2445,10,5566,87,93,17,10,2,87,267,3176,3,82};
+        for (int I = 0; I<myArray.length; I+=1){
+            int numReps = 0;
+            int upperNum = myArray[I]; 
+            for (int i = I; i<myArray.length; i+=1){//May need I+1
+                if (upperNum == myArray[i]){
+                    numReps += 1;
+                }
+            }
+            if (numReps != 0){
+                System.out.println(MessageFormat.format("{I} is repeated {numReps} times", I, numReps));
+            }
+        }
         //you cannot use the array util. Do this one by hand :(
     }
 }
